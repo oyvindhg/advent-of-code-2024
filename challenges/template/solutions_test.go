0e6af742a98515f7713c0de0de623ab0a00e6d8f
Øyvind Harding Gulbrandsen <oyvindhg@gmail.com>
2024-12-01 23:58:01 +0900

    Use correct solver in solution test template

--- challenges/template/solutions_test.go
+++ challenges/template/solutions_test.go
@@ -5,20 +5,22 @@ import (
 )
 
 func TestSolveFirst(t *testing.T) {
-	data := parseInput("input/input.txt")
-
-	var want int = 0
+	data := parseInput("input/test.txt")
 	result := solveFirst(data)
+
+	want := 0
+
 	if result != want {
 		t.Fatalf("Solution 1 = %v, expected %v", result, want)
 	}
 }
 
 func TestSolveSecond(t *testing.T) {
-	data := parseInput("input/input.txt")
+	data := parseInput("input/test.txt")
+	result := solveSecond(data)
 
-	var want int = 0
-	result := solveFirst(data)
+	want := 0
+
 	if result != want {
 		t.Fatalf("Solution 2 = %v, expected %v", result, want)
 	}
